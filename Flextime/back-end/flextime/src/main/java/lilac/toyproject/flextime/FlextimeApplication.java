e1e7c773198dfafe07184674bb0a1f11bbb7e93f
jysaa5 <jysaa5@naver.com>
2021-06-28 17:18:58 +0900

    Study Spring

---
+++ Flextime/back-end/flextime/src/main/java/lilac/toyproject/flextime/FlextimeApplication.java
@@ -0,0 +1,13 @@
+package lilac.toyproject.flextime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlextimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlextimeApplication.class, args);
+	}
+
+}
---
+++ Flextime/back-end/flextime/src/test/java/lilac/toyproject/flextime/FlextimeApplicationTests.java
@@ -0,0 +1,13 @@
+package lilac.toyproject.flextime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlextimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
